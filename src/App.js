371eca6fad5ffb6a1a4d724bf7e741f2ce8729e6
anoffvu <anoffvu@gmail.com>
2019-11-24 04:15:22 -0500

    directional light

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { AmbientLight } from 'three';
 
 class App extends Component {
   spheres = [];
@@ -13,6 +14,7 @@ class App extends Component {
     this.addLights();
     this.startAnimationLoop();
     window.addEventListener('resize', this.handleWindowResize);
+    window.addEventListener('mousemove', this.handleMouseMove);
   }
 
   componentWillUnmount() {
@@ -30,20 +32,33 @@ class App extends Component {
     this.camera.updateProjectionMatrix();
   };
 
+  handleMouseMove = () => {};
+
   sceneSetup = () => {
-    // grab dimensions
+    // grab view variables
     const width = this.mount.clientWidth;
     const height = this.mount.clientHeight;
     const fov = 75;
-    const aspect = width / height; // the canvas default
-    const near = 0.1;
-    const far = 10000;
+    const aspect = width / height; // the canvas default aspect ratio
+    const nearPlane = 0.1;
+    const farPlane = 10000;
 
-    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    // construct the camera
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, nearPlane, farPlane);
     // move camera back a bit
-    this.camera.position.z = 5;
+    this.camera.position.z = 500;
 
     this.scene = new THREE.Scene();
+    // this.scene.background = new THREE.CubeTextureLoader()
+    //   .setPath('resources')
+    //   .load([
+    //     'cave.jpg',
+    //     'cave.jpg',
+    //     'cave.jpg',
+    //     'cave.jpg',
+    //     'cave.jpg',
+    //     'cave.jpg'
+    //   ]);
 
     this.controls = new OrbitControls(this.camera, this.mount);
 
@@ -53,11 +68,17 @@ class App extends Component {
   };
 
   addCustomSceneObjects = () => {
-    var geometry = new THREE.SphereBufferGeometry(150, 32, 16);
-    var material = new THREE.MeshBasicMaterial({
+    var geometry = new THREE.SphereGeometry(150, 100, 100);
+    var geometry2 = new THREE.SphereBufferGeometry(150, 7, 7, 0, 6.3, 0, 5.8);
+    // var material = new THREE.MeshBasicMaterial({
+    //   color: 0xffffff,
+    //   envMap: this.scene.background,
+    //   refractionRatio: 0.95
+    // });
+    var material = new THREE.MeshLambertMaterial({
       color: 0xffffff,
-      envMap: this.scene.background,
-      refractionRatio: 0.95
+      transparent: false,
+      opacity: 0.9
     });
     // material.envMap.mapping = THREE.CubeRefractionMapping;
 
@@ -70,21 +91,37 @@ class App extends Component {
       this.scene.add(mesh);
       this.spheres.push(mesh);
     }
+
+    // for (var i = 0; i < 250; i++) {
+    //   var mesh = new THREE.Mesh(geometry2, material);
+    //   mesh.position.x = Math.random() * 10000 - 5000;
+    //   mesh.position.y = Math.random() * 10000 - 5000;
+    //   mesh.position.z = Math.random() * 10000 - 5000;
+    //   mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+    //   this.scene.add(mesh);
+    //   this.spheres.push(mesh);
+    // }
   };
 
   addLights = () => {
     const lights = [];
-    lights[0] = new THREE.PointLight(0xfffff, 1, 0);
-    lights[1] = new THREE.PointLight(0xfffff, 1, 0);
-    lights[2] = new THREE.PointLight(0xfffff, 1, 0);
+    lights[0] = new THREE.PointLight(0xffffff, 1, 1000);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 1000);
+    lights[2] = new THREE.PointLight(0xffffff, 2, 10000);
 
-    lights[0].position.set(0, 200, 0);
-    lights[0].position.set(100, 200, 100);
-    lights[0].position.set(-100, -200, -100);
+    lights[3] = new THREE.DirectionalLight(0xffffff, 0.7);
 
-    this.scene.add(lights[0]);
-    this.scene.add(lights[1]);
-    this.scene.add(lights[2]);
+    lights[0].position.set(-1000, 0, 0);
+    lights[1].position.set(-1000, 500, 0);
+    lights[2].position.set(-1000, 0, 0);
+    lights[3].position.set(-1000, 0, 0);
+    lights[3].target.position.set(1000, 0, 0);
+
+    this.scene.add(lights[3]);
+
+    // this.scene.add(lights[0]);
+    // this.scene.add(lights[1]);
+    // this.scene.add(lights[2]);
   };
 
   startAnimationLoop = () => {
